7fcfc0840883992894bbfd54f154ddad847cf76c
Mohamed Largo Yagoubi <mohamedlarg@gmail.com>
2025-04-14 18:27:39 +0200

    main page and user login

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mock/', views.mock, name='mock'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def mock(request):
+    return render(request, 'mock.html')
+
+
+
+
+
+
