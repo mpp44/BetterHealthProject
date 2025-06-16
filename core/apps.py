a902ff5335ac4161840484a439a4376269a31199
Maria Azcona <153600270+mariaazcona@users.noreply.github.com>
2025-06-16 18:36:30 +0200

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -5,14 +5,7 @@ from django.db.models.signals import post_migrate
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'   # Debe coincidir con el nombre de la carpeta / módulo
+    name = 'core'   
 
     def ready(self):
-        # 🔹 Registrar señales
-        import core.signals  # noqa
-
-        # (Opcional) Si todavía tienes el hook antiguo aquí, elimínalo:
-        # from .models import StaffUser
-        # import dotenv
-        # dotenv.load_dotenv()
-        # ...
+        import core.signals
